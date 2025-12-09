e7dc25047099c0ae984ea9764cdffd8ce8684997
Carl Axelson <carl.axelson2006@gmail.com>
2025-12-09 12:21:25 +0100

    Revert "nah comnponent basic shi"
    This reverts commit c7067f2c44bd47707b7d643fbff23e7465333cf5.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,39 @@
 <script setup>
 import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
 </script>
 
 <template>
-<nav>
-  <router-link to="/">Home</router-link>
-  <router-link to="/about">About</router-link>
-</nav>
-<router-view />
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <button type="button" @click="count++">count is {{ count }}</button>
+    <p>
+      Edit
+      <code>components/HelloWorld.vue</code> to test HMR
+    </p>
+  </div>
+
+  <p>
+    Check out
+    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
+      >create-vue</a
+    >, the official Vue + Vite starter
+  </p>
+  <p>
+    Learn more about IDE Support for Vue in the
+    <a
+      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
+      target="_blank"
+      >Vue Docs Scaling up Guide</a
+    >.
+  </p>
+  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
 <style scoped>
